7486501632bb2be69f6495afec6de3af6d5677b8
Mohammed Saif <saifmohammed888@gmail.com>
2022-02-24 02:28:30 +0530

    Fixing build

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
                 <meta name="description" content="Get your groceries at your doorstep" />
-                <meta property="og:site_name" content="Ethius Technologies" />
-                <meta property="og:title" content="Ethius Technologies" />
-                <meta property="og:description" content="Providing End to End Solutions to Computer Hardware and Software" />
+                <meta property="og:site_name" content="Grostore" />
+                <meta property="og:title" content="Groceries at doorstep" />
+                <meta property="og:description" content="Get your groceries at your doorstep" />
                 <meta
                     property="og:image"
                     content="https://res.cloudinary.com/saif-freelance2/image/upload/v1645607384/Grostore/logo/GROSTORE-nobg_gxhxpt.webp"
@@ -28,7 +28,7 @@ export default function Home() {
                 <link href="/dist/output.css" rel="stylesheet"></link>
             </Head>
 
-            <main className="bg-red-500 w-screen h-[54vh]">
+            <main>
                 <Welcome />
             </main>
         </div>
