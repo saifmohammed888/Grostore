00041174d3d00a84af071cfa8c840f8cc90d1326
Mohammed Saif <saifmohammed888@gmail.com>
2022-03-03 06:47:56 +0530

    Error page, tsconfig

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,33 @@
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
 const Navbar = dynamic(() => import('components/common/navbar/navbar'));
 
 const Error: React.FC = () => {
   return (
-    <div className="bg-banner  w-screen h-screen">
-      <Navbar />
-      <h1 className="text-4xl font-bold text-white text-center ">
-        404 Page not found
-      </h1>
-    </div>
+    <>
+      <Head>
+        <title>Grostore | Error</title>
+        <meta
+          name="description"
+          content="Get your groceries at your doorstep"
+        />
+        <meta property="og:site_name" content="Grostore" />
+        <meta property="og:title" content="Grostores/Error" />
+        <meta property="og:error" content="404 Page not found" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:updated_time" content="1440432930" />
+      </Head>
+      <main>
+        <div className="bg-banner bg-bottom first-line:w-screen h-screen">
+          <Navbar />
+          <h1 className="text-4xl mt-[20vh] md:mt-1 font-bold text-white text-center ">
+            404 Page not found
+          </h1>
+        </div>
+      </main>
+    </>
   );
 };
 
